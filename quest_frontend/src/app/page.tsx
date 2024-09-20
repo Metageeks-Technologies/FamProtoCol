d7edeba3bdd38a3e5f01d0a753a30e0fac0c626b
shivam <shivam@metageeks.tech>
2024-09-20 10:44:43 +0530

    social link added

--- quest_frontend/src/app/page.tsx
+++ quest_frontend/src/app/page.tsx
@@ -16,6 +16,7 @@ import Swal from "sweetalert2";
 import upgradeableContractAbi from "@/utils/abi/upgradableContract.json";
 import usdc from "@/utils/abi/usdc.json";
 import { useRouter } from "next/navigation";
+import Link from "next/link"
 
 const LandingPage = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -388,37 +389,23 @@ const LandingPage = () => {
                       </h1>
                     </div>
                     <div className="flex row gap-1">
-                      <div className="box1 right-trapezium w-[2rem] h-[2rem] bg-[#ffffff33] p-[1px]">
-                        <svg
-                          className="box2 right-trapezium p-2 bg-[#111111]"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="17"
-                          height="17"
-                          viewBox="0 0 17 17"
-                          fill="none"
-                        >
-                          <path
-                            d="M12.5736 2.125H14.7461L10.0003 7.52604L15.5834 14.875H11.2115L7.78815 10.4175L3.87035 14.875H1.69577L6.7724 9.09854L1.41669 2.125H5.89902L8.99444 6.19933L12.5736 2.125ZM11.8115 13.5802H13.0156L5.24452 3.35183H3.95252L11.8115 13.5802Z"
-                            fill="white"
-                          />
-                        </svg>
+                    {/* twitter */}
+                    <Link target="_blank" href="https://x.com/famprotocol" >
+                      <div className="box1 right-trapezium bg-zinc-700 p-[1px]">
+                        <div className="box2 right-trapezium p-2 bg-[#111111]" >
+                          <i className="bi bi-twitter-x"></i>
+                        </div>
                       </div>
-                      <div className="box1 left-right-trapezium w-[2rem] h-[2rem] bg-[#ffffff33] p-[1px]">
-                        <svg
-                          className="box2 left-right-trapezium p-2 bg-[#111111]"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="17"
-                          height="17"
-                          viewBox="0 0 17 17"
-                          fill="none"
-                        >
-                          <path
-                            d="M13.6496 3.77537C12.7075 3.3362 11.6875 3.01745 10.625 2.83328C10.6157 2.83299 10.6064 2.83473 10.5978 2.83841C10.5893 2.84208 10.5816 2.84758 10.5754 2.85453C10.4479 3.08828 10.2991 3.39287 10.2 3.62662C9.07302 3.45662 7.92694 3.45662 6.79998 3.62662C6.70081 3.38578 6.55206 3.08828 6.41748 2.85453C6.4104 2.84037 6.38915 2.83328 6.3679 2.83328C5.3054 3.01745 4.29248 3.3362 3.34331 3.77537C3.33623 3.77537 3.32915 3.78245 3.32206 3.78953C1.3954 6.67245 0.864148 9.47745 1.12623 12.2541C1.12623 12.2683 1.13331 12.2825 1.14748 12.2895C2.42248 13.2245 3.6479 13.7912 4.85915 14.1666C4.8804 14.1737 4.90165 14.1666 4.90873 14.1525C5.19206 13.7629 5.44706 13.352 5.66665 12.92C5.68081 12.8916 5.66665 12.8633 5.63831 12.8562C5.23456 12.7004 4.85206 12.5162 4.47665 12.3037C4.44831 12.2895 4.44831 12.247 4.46956 12.2258C4.54748 12.1691 4.6254 12.1054 4.70331 12.0487C4.71748 12.0345 4.73873 12.0345 4.7529 12.0416C7.18956 13.1537 9.81748 13.1537 12.2258 12.0416C12.24 12.0345 12.2612 12.0345 12.2754 12.0487C12.3533 12.1125 12.4312 12.1691 12.5091 12.2329C12.5375 12.2541 12.5375 12.2966 12.5021 12.3108C12.1337 12.5304 11.7441 12.7075 11.3404 12.8633C11.3121 12.8704 11.305 12.9058 11.3121 12.927C11.5387 13.3591 11.7937 13.77 12.07 14.1595C12.0912 14.1666 12.1125 14.1737 12.1337 14.1666C13.3521 13.7912 14.5775 13.2245 15.8525 12.2895C15.8666 12.2825 15.8737 12.2683 15.8737 12.2541C16.1854 9.04537 15.3566 6.26162 13.6779 3.78953C13.6708 3.78245 13.6637 3.77537 13.6496 3.77537ZM6.03498 10.5612C5.3054 10.5612 4.69623 9.88828 4.69623 9.05953C4.69623 8.23078 5.29123 7.55787 6.03498 7.55787C6.78581 7.55787 7.38081 8.23787 7.37373 9.05953C7.37373 9.88828 6.77873 10.5612 6.03498 10.5612ZM10.9721 10.5612C10.2425 10.5612 9.63332 9.88828 9.63332 9.05953C9.63332 8.23078 10.2283 7.55787 10.9721 7.55787C11.7229 7.55787 12.3179 8.23787 12.3108 9.05953C12.3108 9.88828 11.7229 10.5612 10.9721 10.5612Z"
-                            fill="#8C71FF"
-                          />
-                        </svg>
+                      </Link>
+                      {/* telegram */}
+                      <Link target="_blank" href="https://t.me/FamProtocol">
+                      <div className="box1 empty-left-trapezium bg-zinc-700 p-[1px]">
+                        <div className="box2 empty-left-trapezium p-2 bg-[#111111]">
+                          <i className="bi bi-telegram"></i>
+                        </div>
                       </div>
-                      <div className="box1 left-trapezium w-[2rem] h-[2rem] bg-[#ffffff33] p-[1px]">
+                      </Link>
+                      {/* <div className="box1 left-trapezium w-[2rem] h-[2rem] bg-[#ffffff33] p-[1px]">
                         <svg
                           className="box2 left-trapezium p-2 bg-[#111111]"
                           xmlns="http://www.w3.org/2000/svg"
@@ -441,7 +428,7 @@ const LandingPage = () => {
                             </clipPath>
                           </defs>
                         </svg>
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                 </div>
